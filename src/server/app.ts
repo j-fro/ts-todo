ea43338d2b2385076365cd719a9047a1f5ac01d0
Jacob Froman <jacob.h.froman@gmail.com>
2017-01-04 21:48:37 -0600

    refatored query code with promises and added db

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,12 +1,14 @@
 import express = require('express');
 import bodyParser = require('body-parser');
 import path = require('path');
+import pg = require('pg');
 let app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-let port = 8000
+const port = 8000
+const connString = 'postgres://localhost:5432/todos'
 
 class Task {
     constructor(private id: number, public name: string, public complete: boolean) {}
@@ -19,13 +21,34 @@ app.get('/', (req, res) => {
 });
 
 app.get('/getTasks', (req, res) => {
-    res.send(tasks);
+    queryTasks().then((result) => {
+        res.send(result);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    });
 })
 
 app.post('/addTask', (req, res) => {
     console.log('Adding task:', req.body);
-    tasks.push(new Task(0, req.body.name, false));
-    res.send(tasks);
+    pg.connect(connString, (err, client, done) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        } else {
+            client.query('INSERT INTO todos (name) VALUES ($1)', [req.body.name]).then(() => {
+                queryTasks().then((result) => {
+                    res.send(result);
+                }).catch((err) => {
+                    console.log(err);
+                    res.sendStatus(500);
+                });
+            }).catch((err) => {
+                console.log(err);
+                res.sendStatus(500);
+            })
+        }
+    })
 });
 
 app.listen(port, () => {
@@ -33,3 +56,24 @@ app.listen(port, () => {
 });
 
 app.use(express.static('built/public'));
+
+function queryTasks(): Promise<Array<Task>> {
+    return new Promise((resolve, reject) => {
+        pg.connect(connString, (err, client, done) => {
+            if(err) {
+                console.log(err);
+                reject(err);
+            } else {
+                client.query('SELECT * FROM todos').then((result) => {
+                    tasks = result.rows.reduce((tasks, row) => {
+                        tasks.push(new Task(row.id, row.name, row.complete));
+                        return tasks
+                    }, Array<Task>());
+                    resolve(tasks);
+                }).catch((err) => {
+                    reject(err);
+                });
+            }
+        });
+    });
+}
